ca016d3612cc5cda663228dca69108f9dae7071c
asc2563 <allistersaintclair@outlook.com>
2023-09-30 20:12:51 -0700

    new aluben update

--- script.js
+++ script.js
@@ -292,11 +292,12 @@ function proxylist() {
 }
 
 window.addEventListener("DOMContentLoaded", function () {
-  // Generate a random number between 0 and 100
-  const randomNumber = Math.floor(Math.random() * 101);
+  // Generate a random number between 0 and 99
+  let randomNumber = Math.floor(Math.random() * 100);
 
-  // Check if the random number is less than or equal to 1
-  if (randomNumber <= 1) {
-    document.getElementById("traf-say").style.display = "block";
+  // Check if the random number equal to 43
+  if (randomNumber == "43") {
+    var poop = document.getElementById("traf-say");
+    poop.style.display = "block";
   }
 });
